150264f5777afd801aa32484312c29ce8f5f044f
VAD <vadprimary@gmail.com>
2020-06-10 13:51:35 +0700

    add editor version next to symbol link file

--- JenkinsFiles/ci/AfterBuildSuccess/ArchiveBuildResults.py
+++ JenkinsFiles/ci/AfterBuildSuccess/ArchiveBuildResults.py
@@ -13,6 +13,7 @@ import Config
 
 project_folder = Config.read_config(Config.KEY.UNITY_PROJECT)
 version = Config.read_config(Config.KEY.RELEASE_VERSION)
+unity_path = Config.read_config(Config.KEY.UNITY_PATH)
 
 build_folder = os.path.join(project_folder, "build")
 
@@ -26,27 +27,33 @@ release_folder = os.path.join(release_folder, version)
 Path(release_folder).mkdir(parents=True, exist_ok=True)
 Path(develop_folder).mkdir(parents=True, exist_ok=True)
 
-for file in os.listdir(build_folder):
-    print("Check File: " + file)
+for f in os.listdir(build_folder):
+    print("Check File: " + f)
     # Send apk to base folder with project name
 
-    if file.endswith(".apk"):
-        apk_file= os.path.join(build_folder, file)
-        new_apk_file = os.path.join(develop_folder, file)
+    if f.endswith(".apk"):
+        apk_file= os.path.join(build_folder, f)
+        new_apk_file = os.path.join(develop_folder, f)
         print(f"Copy: {apk_file} to {new_apk_file}")
         copyfile(apk_file, new_apk_file)
 
     # Send aab + symbol zip to release folder.
-    if file.endswith(".aab"):
-        aab_file= os.path.join(build_folder, file)
-        new_aab_file = os.path.join(release_folder, file)
+    if f.endswith(".aab"):
+        aab_file= os.path.join(build_folder, f)
+        new_aab_file = os.path.join(release_folder, f)
         print(f"Copy: {aab_file} to {new_aab_file}")
         copyfile(aab_file, new_aab_file)
-    if "symbols.zip" in file:
-        symbol_file = os.path.join(build_folder, file)
-        new_symbol_file = os.path.join(release_folder, file)
+    if "symbols.zip" in f:
+        symbol_file = os.path.join(build_folder, f)
+        new_symbol_file = os.path.join(release_folder, f)
         print(f"Copy: {symbol_file} to {new_symbol_file}")
         copyfile(symbol_file, new_symbol_file)
+        print("Create txt helper to know unity version when debug symbol link")
+        editor_hint_file = os.path.join(build_folder, f +"." + "editor.txt")
+        with open(editor_hint_file, "w") as file:
+            file.write(unity_path)
+        
+
 
 
 
