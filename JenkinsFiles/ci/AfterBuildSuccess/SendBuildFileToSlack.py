98e47b547683e6366c11eaf807ce72e470dff0d2
VAD <vadprimary@gmail.com>
2020-11-27 17:11:02 +0700

    stop slack from sending file apk Send direct link instead

--- JenkinsFiles/ci/AfterBuildSuccess/SendBuildFileToSlack.py
+++ JenkinsFiles/ci/AfterBuildSuccess/SendBuildFileToSlack.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from urllib.parse import quote
+import pathlib
 
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
@@ -30,14 +32,19 @@ branch = os.environ["BRANCH_NAME"]
 build_id = os.environ["BUILD_NUMBER"]
 pipeline_url = os.environ["RUN_DISPLAY_URL"]
 build_folder = os.path.join(unity_project, "build")
+build_url = os.environ["BUILD_URL"]
+workspace = os.environ["WORKSPACE"]
+artifact_url = build_url + "artifact"
 
 # Not use function def due to function not get local variable run in jenkins
 for (dirpath, dirnames, filenames) in os.walk(build_folder):
     for file in filenames:
-        print(file)
         if file.endswith(".apk"):
             apk_file = os.sep.join([dirpath, file])
-            print("Found: " + apk_file)
+            ws = pathlib.Path(workspace)
+            p = pathlib.Path(apk_file)            
+            full_url =  f"{artifact_url}/{quote('/'.join( p.parts[len(ws.parts):]))}"
+            print(full_url)
             msg = f'''\
 *{company}|{project}*
 {commit_time}
@@ -45,18 +52,25 @@ for (dirpath, dirnames, filenames) in os.walk(build_folder):
 ```{git_full_message}```
 Unity build *SUCCESS*
 Detail: {pipeline_url}
+Download: {full_url}
 '''
-            SlackCommand.send_file(slack_channel, apk_file, f"{file}", msg)
+            SlackCommand.send_message(slack_channel, msg)
+            #SlackCommand.send_file(slack_channel, apk_file, f"{file}", msg)
 
         if file.endswith(".aab"):
             aab_file = os.sep.join([dirpath, file])
-            print("Found: " + aab_file)
+            ws = pathlib.Path(workspace)
+            p = pathlib.Path(aab_file)            
+            full_url =  f"{artifact_url}/{quote('/'.join( p.parts[len(ws.parts):]))}"
+            print(full_url)
             msg = f'''\
 *{company}|{project}*
 {commit_time}
 {build_id} - {committer} | {branch}-{git_hash}
 ```{git_full_message}```
-Unity build production *SUCCESS*
+Unity production build *SUCCESS*
 Detail: {pipeline_url}
+Download: {pipeline_url}
 '''
-            SlackCommand.send_file(slack_channel, aab_file, f"{file}", msg)
+            SlackCommand.send_message(slack_channel, msg)
+            #SlackCommand.send_file(slack_channel, aab_file, f"{file}", msg)
